c63e2bce707b932f30bc5bed5a7a7b3ebf39379e
Ian-Ngari <ianngari02@gmail.com>
2025-11-11 10:47:06 +0300

    company and culture removed

--- components/landing-page/footer.tsx
+++ components/landing-page/footer.tsx
@@ -48,9 +48,9 @@ export default function Footer() {
           <div className="space-y-4">
             <h3 className="text-lg font-semibold mb-6">Company</h3>
             <nav className="flex flex-col space-y-4">
-              <Link href="/company-and-culture" className="text-gray-400 hover:text-white transition-colors">
+              {/* <Link href="/company-and-culture" className="text-gray-400 hover:text-white transition-colors">
                 Company & Culture
-              </Link>
+              </Link> */}
               <Link href="#" className="text-gray-400 hover:text-white transition-colors">
                 Careers
               </Link>
